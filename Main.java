3f7dc1c1549092cf3b94304662e0c31a9a37486f
Neagrau Seba <seba.ng@yahoo.com>
2019-12-17 16:33:27 +0200

    calculator

---
+++ Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    calc(4,3,'+');
+    calc(3.5,2,'-');
+    calc(3.5,2,'*');
+    calc(3.5,2,'/');
+    calc(3,2,'%');
+
+
+    }
+    public static void calc(double x, double y,char c)
+    {
+        switch(c) {
+            case '+':
+                System.out.println("x + y = " + (x + y));
+                break;
+            case '-':
+                System.out.println("x - y = " + (x - y));
+                break;
+            case '*':
+                System.out.println("x * y = " + (x * y));
+                break;
+            case '/':
+                System.out.println("x / y = " + (x / y));
+                break;
+            case '%':
+                System.out.println("x % y = " + (x % y));
+                break;
+             default :
+                System.out.println("Not a valid operation");
+                break;
+        }
+
+    }
+
+}
+
+
+
+
+
